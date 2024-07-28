516104f23e74a92d3f479b3bda1a68aca9776f13
Daniyar Alzhanov <Alorealdo@yandex.ru>
2024-07-28 15:38:01 +0300

    Add get, update and delete methods

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -63,3 +63,30 @@ async def test_get_user(client, create_user_in_database, get_user_from_database)
     assert user_from_resp['surname'] == user_data['surname']
     assert user_from_resp['email'] == user_data['email']
     assert user_from_resp['is_active'] == user_data['is_active']
+
+
+async def test_update_user(client, create_user_in_database, get_user_from_database):
+    user_data = {
+        'user_id':uuid4(),
+        'name': 'Daniyar',
+        'surname': 'Alzhanov',
+        'email': 'mail@mail.ru',
+        'is_active':True
+    }
+    user_data_updated = {
+        'name': 'Ivan',
+        'surname': 'Pushkov',
+        'email': 'test@mail.ru',
+    }
+    await create_user_in_database(**user_data)
+    resp = client.patch(f'/user/?user_id={user_data['user_id']}', data=json.dumps(user_data_updated))
+    assert resp.status_code == 200
+    resp_data = resp.json()
+    assert resp_data['updated_user_id'] == str(user_data['user_id'])
+    users_from_db = await get_user_from_database(user_data['user_id'])
+    user_from_db = dict(users_from_db[0])
+    assert users_from_db['name'] == user_data_updated['name']
+    assert users_from_db['surname'] == user_data_updated['surname']
+    assert users_from_db['email'] == user_data_updated['email']
+    assert users_from_db['is_active'] is user_data['is_active']
+    assert user_from_db['user_id'] == user_data['user_id']
